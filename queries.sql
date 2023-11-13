c84e27cd9e4528cc987e152029bdf2d8b9a22e15
NanitaAgarwal <nanita.agarwal@gmail.com>
2023-11-13 17:03:18 -0600

    Graded Assignment # 4

--- queries.sql
+++ queries.sql
@@ -1,7 +1,30 @@
---Part 1
+-- Part 1
+-- id int PK
+-- employer varchar(255)
+-- name varchar(255)
+-- skills varchar(255)
 
---Part 2
+-- Part 2
+SELECT name
+FROM employer
+WHERE location = "St. Louis City";
 
---Part 3
+-- Part 3
+DROP TABLE job;
 
---Part 4
+-- Part 4
+-- Select Distinct(S.Name)
+--   From Skill S
+--        Inner Join Job_Skills JS On JS.Skills_Id = S.Id
+--  Order By S.Name;
+
+--Select *
+--  From Skill
+--       Inner Join Job_Skills On Job_Skills.Skills_Id = Skill.Id
+-- Order By Skill.Name Asc;
+
+ Select *
+   From Skill
+        Inner Join Job_Skills On Skill.Id = Job_Skills.Skills_Id
+  Where Job_Skills.Jobs_Id Is Not Null
+ Order By Name Asc;
